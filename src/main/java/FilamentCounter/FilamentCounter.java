3a5a5f70590e8f6923f6ac36cb1e9b265461e600
janositibor <janositibor@gmail.com>
2023-05-02 14:00:28 +0200

    Directory found

--- src/main/java/FilamentCounter/FilamentCounter.java
+++ src/main/java/FilamentCounter/FilamentCounter.java
@@ -1,5 +1,8 @@
 package FilamentCounter;
 
+//import javafx.stage.DirectoryChooser;
+import ij.io.DirectoryChooser;
+import ij.io.LogStream;
 import net.imagej.Dataset;
 import net.imagej.ImageJ;
 import net.imagej.ops.OpService;
@@ -11,63 +14,56 @@ import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
 import org.scijava.ui.UIService;
 
+
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
 @Plugin(type = Command.class, menuPath = "Plugins>Filament Counter")
 public class FilamentCounter<T extends RealType<T>> implements Command {
-        //
-        // Feel free to add more parameters here...
-        //
+    //
+    // Feel free to add more parameters here...
+    //
 
-        @Parameter
-        private Dataset currentData;
+//    @Parameter
+//    private Dataset currentData;
 
-        @Parameter
-        private UIService uiService;
+    @Parameter
+    private UIService uiService;
 
-        @Parameter
-        private OpService opService;
+    @Parameter
+    private OpService opService;
 
-        @Override
-        public void run() {
-            final Img<T> image = (Img<T>) currentData.getImgPlus();
+    @Override
+    public void run() {
+//        Opener fo = new Opener();
+        DirectoryChooser dc = new DirectoryChooser("Choose directory");
 
-            //
-            // Enter image processing code here ...
-            // The following is just a Gauss filtering example
-            //
-            final double[] sigmas = {1.0, 3.0, 5.0};
+        String directory=dc.getDirectory();
 
-            List<RandomAccessibleInterval<T>> results = new ArrayList<>();
+//        LogStream logStream=public LogStream();
+        System.out.println(directory);
 
-            for (double sigma : sigmas) {
-                results.add(opService.filter().gauss(image, sigma));
-            }
+//        DirectoryChooser chooser = new DirectoryChooser();
 
-            // display result
-            for (RandomAccessibleInterval<T> elem : results) {
-                uiService.show(elem);
-            }
-        }
-        public static void main(final String... args) throws Exception {
-            // create the ImageJ application context with all available services
-            final ImageJ ij = new ImageJ();
-            ij.ui().showUI();
 
-            // ask the user for a file to open
-            final File file = ij.ui().chooseFile(null, "open");
-
-            if (file != null) {
-                // load the dataset
-                final Dataset dataset = ij.scifio().datasetIO().open(file.getPath());
-
-                // show the image
-                ij.ui().show(dataset);
-
-                // invoke the plugin
-                ij.command().run(FilamentCounter.class, true);
-            }
-        }
+//        final Img<T> image = (Img<T>) currentData.getImgPlus();
+//
+//        //
+//        // Enter image processing code here ...
+//        // The following is just a Gauss filtering example
+//        //
+//        final double[] sigmas = {1.0, 3.0, 5.0};
+//
+//        List<RandomAccessibleInterval<T>> results = new ArrayList<>();
+//
+//        for (double sigma : sigmas) {
+//            results.add(opService.filter().gauss(image, sigma));
+//        }
+//
+//        // display result
+//        for (RandomAccessibleInterval<T> elem : results) {
+//            uiService.show(elem);
+//        }
+    }
 }
---
+++ src/main/java/FilamentCounter/Main.java
@@ -0,0 +1,28 @@
+package FilamentCounter;
+
+import net.imagej.Dataset;
+import net.imagej.ImageJ;
+
+import java.io.File;
+
+public class Main {
+    public static void main(final String... args) throws Exception {
+        // create the ImageJ application context with all available services
+        final ImageJ ij = new ImageJ();
+        ij.ui().showUI();
+
+        // ask the user for a file to open
+        final File file = ij.ui().chooseFile(null, "open");
+
+        if (file != null) {
+            // load the dataset
+            final Dataset dataset = ij.scifio().datasetIO().open(file.getPath());
+
+            // show the image
+            ij.ui().show(dataset);
+
+            // invoke the plugin
+            ij.command().run(FilamentCounter.class, true);
+        }
+    }
+}
